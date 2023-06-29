25f42532bc210942a0ba69804b583a96fc6abfd7
AditiR28 <rajput.28aditi@gmail.com>
2023-06-29 18:28:10 +0530

    api automation learning

---
+++ src/test/java/API_automation/Restassured/First_TC.java
@@ -0,0 +1,11 @@
+package API_automation.Restassured;
+import io.restassured.RestAssured ;
+
+public class First_TC {
+
+	public static void main(String[] args) {
+		System.out.println("test");;;
+		
+	}
+
+}
---
+++ src/test/java/API_automation/Restassured/testing.java
@@ -0,0 +1,10 @@
+package API_automation.Restassured;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
